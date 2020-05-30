53c4e1d3ea04a074dda9d3d9feda18752fc510dd
Daren <darenleongzy@gmail.com>
2020-05-30 11:50:33 +0800

    added material ui dashboard template

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 // Set up your application entry point here...
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+import Dashboard from './components/dashboard'
+
+function App() {
+  return (
+    // <Button variant="contained" color="primary">
+    //   Hello World
+    // </Button>
+    <Dashboard />
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'));
